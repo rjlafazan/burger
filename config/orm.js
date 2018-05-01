6dd28417e0bb158096448ee2d3eea005b557535c
Ryan <rjlafazan@gmail.com>
2018-04-30 21:58:51 -0700

    orm selectAll function made

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,13 @@
 var connection = require("../config/connection.js");
 
+var orm = {
+    selectAll: function (table, cb) {
+        var queryString = 'SELECT * FROM ' + table + ';';
+		connection.query(queryString, function(err, result){
+			if(err)throw err;
+			cb(result);
+   
+    }
+}
+
 module.exports = orm;
