72d0b5e94d670cdecddbad427cc338e27814f592
Ryan <rjlafazan@gmail.com>
2018-04-30 22:08:22 -0700

    orm insertOne function made

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,21 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: function (table, cb) {
-        var queryString = 'SELECT * FROM ' + table + ';';
-		connection.query(queryString, function(err, result){
+  selectAll: function(table, cb) {
+    var queryString = "SELECT * FROM " + table + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne(): function(table, col, vals, cb) {
+      var queryString ='INSERT INTO ' + table + ' (' + col + ') ' + 'VALUES (' + '?' + ')';
+
+		connection.query(queryString, vals, function(err, result){
 			if(err)throw err;
-			cb(result);
-   
-    }
-}
+			cb(err, result);
+		});
+  }
+};
 
 module.exports = orm;
